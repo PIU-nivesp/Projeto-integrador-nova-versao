8e92f696bb64856047fad14ecc4a47c36427f5a4
RenatoApache <renatodtsato@gmail.com>
2025-11-15 13:28:57 -0300

    chore: Restruturação do .gitignore e remoção de arquivos temporários do rastreamento

--- pisite/pisite/urls.py
+++ pisite/pisite/urls.py
@@ -1,27 +1,31 @@
 """
 URL configuration for pisite project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The `urlpatterns` list routes URLs to views.
+For more information, see: https://docs.djangoproject.com/en/5.2/topics/http/urls/
 """
 from django.contrib import admin
 from django.urls import path
 from catalogo import views
 
 urlpatterns = [
+    # URLs do Django Admin
     path('admin/', admin.site.urls),
+
+    # URLs da Aplicação 'catalogo'
+    
+    # Rota raiz: Mapeada para o login, nomeada como 'login' para redirecionamentos.
     path('', views.login_view, name='login'),
+    
+    # Rota para a página principal (Home)
     path('home', views.index, name='index'),
+    
+    # Rota para a página de estoque
     path('estoque', views.estoque, name='estoque'),
+    
+    # Rota para a página de inserção de dados
     path('inserir', views.inserir, name='inserir'),
+
+    # Rota para o logout (limpa a sessão)
+    path('logout', views.logout_view, name='logout'),
 ]
